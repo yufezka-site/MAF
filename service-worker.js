1a6727978aa5d341d0dc60e13ad00aa3b7ed2a3a
yufezka-site <yufezka@gmail.com>
2025-10-03 10:13:16 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "maf-tracker-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch cache-first
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update cache
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
